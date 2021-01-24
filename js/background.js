a8acb2ba73d922e18bb0d530e979b207b64ac6a2
Paul Smith <paulrsmithjnr@gmail.com>
2021-01-24 10:52:21 -0500

    Created regex for the recording url that works

---
+++ js/background.js
@@ -0,0 +1,12 @@
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        // console.log(current_tab_info.url);
+        var regex = /^https:\/\/ca\.bbcollab\.com\/collab\/ui\/session\/playback/;
+        if(regex.test(current_tab_info.url)) {
+            console.log("Found a BbC recording!!");
+            // var video = document.getElementById("playback-video-playback-video_html5_api");
+            // alert(video.src);
+            // chrome.tabs.executeScript(null, {file: 'js/popup.js'}, () => console.log("popup.js successfully loaded!"));
+        }
+    });
+});
---
+++ js/popup.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function() {
+    
+//     document.getElementById("text").innerHTML = "BbC recording found!";
+//     document.getElementById("modal-button").innerHTML = "<button id=\"download\">Download</button>";
+
+//     document.getElementById("download").addEventListener('click', onclick, false);
+
+//     function onclick() {
+//         var video = document.getElementById("playback-video-playback-video_html5_api");
+//         alert(video.src);
+//         // chrome.tabs.query({currentWindow: true, active: true},
+//         // function(tabs) {
+//         //     chrome.tabs.sendMessage(tabs[0].id, 'match');
+//         // })
+//     }
+// });

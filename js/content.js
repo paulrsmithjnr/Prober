2cb5729f01d95d2cb92e267144f701a64ba295f0
Paul Smith <paulrsmithjnr@gmail.com>
2021-01-31 00:36:01 -0500

    Successfully found video and logged it to the console

--- js/content.js
+++ js/content.js
@@ -1,4 +1,29 @@
-chrome.runtime.onMessage.addListener(function(request) {
-    alert(request);
-    
+// const videoRec = document.getElementById("playback-video-playback-video_html5_api");
+// chrome.runtime.sendMessage({
+//     video: videoRec
+// });
+// console.log(video);
+console.log("The content script works as it should");
+
+var videoSelector = "playback-video-playback-video_html5_api";
+var observer = new MutationObserver(function(mutations) {
+    // console.log(mutations);
+    mutations.forEach((mutation) => {
+        for(var i=0; i<mutation.addedNodes.length; i++) {
+            if(mutation.addedNodes[i].id == videoSelector) {
+                console.log("Video found!");
+                observer.disconnect();
+                const videoRec = document.getElementById("playback-video-playback-video_html5_api");
+                // chrome.runtime.sendMessage({
+                //     video: videoRec
+                // });
+                console.log(videoRec);
+            }
+        }
+    });
+});
+
+observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
 });
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     
-//     document.getElementById("text").innerHTML = "BbC recording found!";
-//     document.getElementById("modal-button").innerHTML = "<button id=\"download\">Download</button>";
+    // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+    //     const video = request.videoRec;
+    //     document.getElementById("textContent").innerHTML = "BbC recording found!";
+    //     console.log("Actually found the video");
+    //     console.log(video);
+    // });
+    // document.getElementById("text").innerHTML = "BbC recording found!";
+    // document.getElementById("modal-button").innerHTML = "<button id=\"download\">Download</button>";
 
-//     document.getElementById("download").addEventListener('click', onclick, false);
+    // document.getElementById("download").addEventListener('click', onclick, false);
 
-//     function onclick() {
-//         var video = document.getElementById("playback-video-playback-video_html5_api");
-//         alert(video.src);
-//         // chrome.tabs.query({currentWindow: true, active: true},
-//         // function(tabs) {
-//         //     chrome.tabs.sendMessage(tabs[0].id, 'match');
-//         // })
-//     }
-// });
+    // function onclick() {
+    //     var video = document.getElementById("playback-video-playback-video_html5_api");
+    //     alert(video.src);
+    //     // chrome.tabs.query({currentWindow: true, active: true},
+    //     // function(tabs) {
+    //     //     chrome.tabs.sendMessage(tabs[0].id, 'match');
+    //     // })
+    // }
+});

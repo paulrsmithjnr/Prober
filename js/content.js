5c84d46dd6b3d18d5325b2a620a23fd429156bca
Paul Smith <paulrsmithjnr@gmail.com>
2021-01-31 16:50:46 -0500

    Made a lot of changes. Added download icon to panel. Still needs lots of fixing

--- js/content.js
+++ js/content.js
@@ -8,16 +8,50 @@ console.log("The content script works as it should");
 var videoSelector = "playback-video-playback-video_html5_api";
 var observer = new MutationObserver(function(mutations) {
     // console.log(mutations);
+    var foundFlag = false;
     mutations.forEach((mutation) => {
         for(var i=0; i<mutation.addedNodes.length; i++) {
             if(mutation.addedNodes[i].id == videoSelector) {
                 console.log("Video found!");
-                observer.disconnect();
+                console.log(mutation);
+                // observer.disconnect();
                 const videoRec = document.getElementById("playback-video-playback-video_html5_api");
-                chrome.runtime.sendMessage({
-                    video: videoRec
-                });
+                // var controls = document.getElementsByClassName("playback-controls");
+                // console.log(controls);
+                // buttonDivElement = menubarElement.insertBefore(createSpeedControlButton(), menubarElement.querySelector(".fullscreen-toggle"));
+                // chrome.runtime.sendMessage({
+                //     video: videoRec
+                // });
                 console.log(videoRec);
+            }
+            if(mutation.target.classList.contains("playback-controls") && !foundFlag) {
+                console.log("Found playback controls mutation");
+                console.log(mutation.target);
+                foundFlag = true;
+
+                var downloadDiv = document.createElement("div");
+                var downloadButton = document.createElement("button");
+                var downloadIcon = document.createElement("IMG");
+
+                downloadIcon.src = "https://snz04pap002files.storage.live.com/y4mA09MOtZ1jC_w6DfYoPD1BxqweB623J391gxc_f0CYXigLOcySvHjTBctnf_znS_spsGu83kg4Qk9FCu3eWseHIjYT2s8AoqNxfApNHyTw5Us8zalcCCXX-1ayWkCMKdNdUKG1H6rNwYpKWtKNUrYZQhVBf6mqsotqOHPp8x0mhOrvzG2SdRGiTQA3i6M-KKQ?width=128&height=128&cropmode=none";
+                downloadButton.classList.add("playback-controls__button", "button", "has-tooltip");
+                downloadIcon.classList.add("playback-controls__button", "button", "has-tooltip");
+
+                downloadButton.appendChild(downloadIcon);
+                downloadDiv.appendChild(downloadButton);
+
+                // var downloadIcon = document.createElement("IMG");
+                // downloadIcon.src = "https://snz04pap002files.storage.live.com/y4mA09MOtZ1jC_w6DfYoPD1BxqweB623J391gxc_f0CYXigLOcySvHjTBctnf_znS_spsGu83kg4Qk9FCu3eWseHIjYT2s8AoqNxfApNHyTw5Us8zalcCCXX-1ayWkCMKdNdUKG1H6rNwYpKWtKNUrYZQhVBf6mqsotqOHPp8x0mhOrvzG2SdRGiTQA3i6M-KKQ?width=128&height=128&cropmode=none";
+
+                // var newItem = document.createElement("LI");       // Create a <li> node
+                // var textnode = document.createTextNode("Water");  // Create a text node
+                // newItem.appendChild(textnode);
+                
+                var fs = document.getElementsByClassName("fullscreen-toggle ng-scope")[0];
+                mutation.target.insertBefore(downloadIcon, fs);
+
+                observer.disconnect();
+                break;
             }
         }
     });
